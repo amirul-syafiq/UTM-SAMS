d64460c41f5e780d9ed22d2fdbdf8fb0468b14d7
Amirul Syafiq <syafiq2000@outlook.com>
2023-04-16 23:51:56 +0800

    flowbite framework added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,16 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: "media",
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./node_modules/flowbite/**/*.js"
+
     ],
+
 
     theme: {
         extend: {
@@ -17,5 +21,10 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'),
+
+              require('flowbite/plugin')
+
+             ],
+
 };
