a24c41c9c88368a6a82e7ad0c85471aef9777aee
Amirul Syafiq <syafiq2000@outlook.com>
2023-05-03 02:00:04 +0800

    add custom table component

---
+++ resources/views/components/custom-table.blade.php
@@ -0,0 +1,30 @@
+<!-- components/table.blade.php -->
+@props(['data'])
+
+@php
+    $headers = $data->first()->getFillable();
+@endphp
+
+<table {{ $attributes->merge(['class' => 'min-w-full divide-y divide-gray-200']) }}>
+    <thead class="bg-gray-50 dark:bg-gray-800">
+        <tr>
+            @foreach ($headers as $header)
+                <th scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    {{ $header }}
+                </th>
+            @endforeach
+        </tr>
+    </thead>
+    <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-700">
+        @foreach ($data as $item)
+            <tr class="hover:bg-gray-100 dark:hover:bg-gray-600">
+                @foreach ($headers as $header)
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {{ $item->$header }}
+                    </td>
+                @endforeach
+            </tr>
+        @endforeach
+    </tbody>
+</table>
