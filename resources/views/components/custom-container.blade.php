38085e3eceb464328144c6f08bf4871aeef74311
Amirul Syafiq <syafiq2000@outlook.com>
2023-05-22 23:01:00 +0800

    add custom-container component

---
+++ resources/views/components/custom-container.blade.php
@@ -0,0 +1,5 @@
+{{-- This component is used to set a container for any purposes --}}
+<div class= 'max-w-3xl mx-auto my-6 px-4 py-8 bg-white dark:bg-gray-800 shadow rounded-md'>
+   {{ $slot }}
+
+</div>
