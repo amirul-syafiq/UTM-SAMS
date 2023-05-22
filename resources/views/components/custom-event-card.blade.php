bac4f117fc9fa066710a389c323d8f6fcbf9a555
Amirul Syafiq <syafiq2000@outlook.com>
2023-05-22 23:45:49 +0800

    update how image is displayed

--- resources/views/components/custom-event-card.blade.php
+++ resources/views/components/custom-event-card.blade.php
@@ -1,26 +1,36 @@
 <div class="bg-white rounded-lg shadow-lg overflow-hidden">
-    <div class="h-64 bg-gray-400"
-
-    style="background-image: url('{{ $event->eventAdvertisementImage->imageUrl }}'); background-size: cover; background-position: center;">
+    <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+        <div class="h-64 relative">
+            <div class="absolute inset-0">
+                <img src="{{ $event->eventAdvertisementImage->imageUrl }}" alt="Image" class="h-full w-full object-cover bg-white blur-sm	">
+            </div>
+            <div class="absolute inset-0 flex items-center justify-center">
+                <img src="{{ $event->eventAdvertisementImage->imageUrl }}" alt="Image" class="max-h-full max-w-full object-contain">
+            </div>
+        </div>
     </div>
-    <div class="p-4">
 
+
+
+    <div class="p-4">
         <h3 class="font-semibold text-lg mb-2">{{ $event->advertisement_title }}</h3>
         <h4 class="font-semibold text-lg mb-2">{{ $event->event->event_name }}</h4>
         <div class="text-gray-600 text-sm mb-2">
-            {{ \Carbon\Carbon::parse($event->event->event_start_date)->toDayDateTimeString() }}</div>
+            {{ \Carbon\Carbon::parse($event->event->event_start_date)->toDayDateTimeString() }}
+        </div>
         <div class="text-gray-600 text-sm mb-2">{{ $event->event->event_venue }}</div>
         <p class="text-gray-700 text-sm">{{ $event->advertisement_description }}</p>
-        <p> tags:
-
+        <p>
+            tags:
             @foreach ($event->tags as $tag)
-                <span class="text-gray-700 text-sm">#{{ $tag->tag_name }} </span>
-                @if (!$loop->last),@endif
+                <span class="text-gray-700 text-sm">#{{ $tag->tag_name }}</span>
+                @if (!$loop->last)
+                    ,
+                @endif
             @endforeach
         </p>
     </div>
-    <div class=" align-text-bottom p-3">
-
+    <div class="align-text-bottom p-3">
         {{ $slot }}
     </div>
 </div>
