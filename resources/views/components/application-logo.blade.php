80ef542a81fd3237635457b287836cb99066aaa1
Amirul Syafiq <syafiq2000@outlook.com>
2023-06-30 20:10:55 +0800

    update asset image with image s3 url

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="/storage/images/LOGO-UTM.png" alt="logo utm" class="w-21 h-10">
+    <img src="https://utmsamsbucket.s3.ap-southeast-1.amazonaws.com/asset/LOGO-UTM.png" alt="logo utm" class="w-21 h-10">
 </a>
--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,5 +1,5 @@
 <a href="/">
     <svg class="w-16 h-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <img src="/storage/images/LOGO-UTM.png" alt="logo utm" class="w-21 h-16">
+        <img src="https://utmsamsbucket.s3.ap-southeast-1.amazonaws.com/asset/LOGO-UTM.png" alt="logo utm" class="w-21 h-16">
     </svg>
 </a>
